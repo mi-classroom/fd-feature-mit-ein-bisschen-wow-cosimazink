49c447fbbe352223b152aa7be7af832204c943c0
cosimazink <cosima_hiromi.zink@smail.th-koeln.de>
2024-05-08 12:37:10 +0200

    addObserver vervollständigt

--- assets/scripts/modules/observer.js
+++ assets/scripts/modules/observer.js
@@ -2,7 +2,22 @@
 ---------------------------------------------------------------------------- */
 
 const addObserver = () => {
+    const observedElements = document.querySelectorAll("[data-js-observe]");
+    const options = {margin: "0px", threshold: 0.5};
 
+    const Observer = new IntersectionObserver (function (entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is-in-view");
+            } else {
+                entry.target.classList.remove("is-in-view");
+            }
+        });
+    }, options);
+    
+    observedElements.forEach ( function (observedElements) {  
+        Observer.observe (observedElements);
+    });
 }
 
 
