3f20e286479fbc4b19651a11bbc8e9551d9b619a
cosimazink <cosima_hiromi.zink@smail.th-koeln.de>
2024-05-08 15:01:41 +0200

    scrollen

--- assets/scripts/modules/keyboard-navigation.js
+++ assets/scripts/modules/keyboard-navigation.js
@@ -2,7 +2,57 @@
 ---------------------------------------------------------------------------- */
 
 const addKeyboardEvents = () => {
-  
+
+    const sections = documents.querySelectorAll('main > section');
+    const lis = document.querySelectorAll('main > section > ul > li');
+    let currentSection = 0;
+    let currentLi = 0;
+
+    const jump = (direction) => {
+        if(direction === "up" && currentSection > 0){
+            currentSection--;
+        }
+
+        if(direction === "down" && currentSection < sections.length - 1){
+            currentSection++;
+        }
+
+        if(direction === "right" && currentSection === 1){
+            currentLi++;
+        }
+
+        lis[currentSection].scrollIntoView({
+            behavior: "smooth"
+        });
+
+        sections[currentSection].scrollIntoView({
+            behavior: "smooth"
+        });
+    };
+
+    document.addEventListener("keydown", (event) => {
+
+        if(event.key === "ArrowUp"){
+            event.preventDefault();
+            event.stopPropagation();
+            jump("up");
+        }
+
+        if(event.key === "ArrowDown"){
+            event.preventDefault();
+            event.stopPropagation();
+            jump("down");
+        }
+
+        if(event.key === "ArrowRight"){
+            jump("right");
+        }
+
+        if(event.key === "ArrowLeft"){
+            jump("left");
+        }
+
+    });
 };
 
 
